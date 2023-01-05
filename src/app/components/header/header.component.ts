57c6242f0a66721f187a68d975868f7da9c7cfc0
Abel <abelperez218@gmail.com>
2023-01-05 18:36:09 -0300

    Header-listo

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+// @ts-ignore
+import Typewriter from 't-writer.js';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+
+    constructor(){ }
+
+    ngOnInit():void {
+      const target = document.querySelector('.tw')
+      const writer = new Typewriter(target, {
+        loop: true, 
+        typeColor: 'white' 
+      })
+      
+      writer
+        .strings(
+          600,
+          "Argentina Programa",
+          "#YoProgramo", 
+          
+        )
+        .start()
+            
+    }
+
+}
